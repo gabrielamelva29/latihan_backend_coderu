87e2f3511abc675f9285937cbd5a1c08161b6132
Gabriela Naibaho <gabriela.naibaho@gdn-commerce.com>
2022-11-09 21:57:13 +0700

    first commit

---
+++ W01S02/century_from_year.js
@@ -0,0 +1,7 @@
+function century(year) {
+    return Math.ceil(year/100);
+ }
+
+ console.log(century(1705)) //'Testing for year 1705');
+ console.log(century(1900)) //, 'Testing for year 1900');
+ console.log(century(1601)) // 'Testing for year 1601');
