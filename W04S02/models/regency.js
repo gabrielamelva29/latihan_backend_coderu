e10850f5049a8f35b5de041757adf53a394f32c5
Gabriela Naibaho <gabriela.naibaho@gdn-commerce.com>
2022-12-12 22:14:47 +0700

    Updated W05S01

--- W04S02/models/regency.js
+++ W04S02/models/regency.js
@@ -10,7 +10,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      this.belongsTo(models.Province, {
+        foreignKey:"province_id",
+        as:"province"
+      })
     }
   }
   Regency.init({
